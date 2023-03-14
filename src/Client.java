a75de4bb387fd1ec7825217f4467c9daa8d3a7c5
Mohamedkelany123 <mohamedkelany479@gmail.com>
2023-03-14 19:23:14 +0200

    HashMaps Changed to ArrayList

--- src/Client.java
+++ src/Client.java
@@ -5,61 +5,83 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    private static final String IP_ADDRESS = "localhost";
-    private static final int PORT = 8888;
+    private static final String HOST = "localhost";
+    private static final int PORT = 12345;
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(IP_ADDRESS, PORT);
-        System.out.println("Connected to server: " + socket);
+        Socket socket = new Socket(HOST, PORT);
 
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+        BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Welcome to the chat app!");
+
         while (true) {
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Logout");
-            System.out.println("4. View all users");
-            BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
-            int choice = Integer.parseInt(consoleIn.readLine());
-    
-            switch (choice) {
-                case 1:
-                    System.out.print("Name: ");
+            System.out.println("Enter a command (REGISTER, LOGIN, LOGOUT, VIEW_USERS):");
+            String command = consoleIn.readLine();
+
+            switch (command) {
+                case "REGISTER":
+                    System.out.println("Enter your name:");
                     String name = consoleIn.readLine();
-                    System.out.print("Username: ");
+                    System.out.println("Enter a username:");
                     String username = consoleIn.readLine();
-                    System.out.print("Password: ");
+                    System.out.println("Enter a password:");
                     String password = consoleIn.readLine();
+
                     out.println("REGISTER");
                     out.println(name);
                     out.println(username);
                     out.println(password);
+
                     System.out.println(in.readLine());
                     break;
-                case 2:
-                    System.out.print("Username: ");
+                case "LOGIN":
+                    System.out.println("Enter your username:");
                     username = consoleIn.readLine();
-                    System.out.print("Password: ");
+                    System.out.println("Enter your password:");
                     password = consoleIn.readLine();
+
                     out.println("LOGIN");
                     out.println(username);
                     out.println(password);
-                    System.out.println(in.readLine());
+
+                    String response = in.readLine();
+
+                    if (response.equals("User authenticated.")) {
+                        System.out.println(response);
+
+                        while (true) {
+                            System.out.println("Enter a message (LOGOUT to exit):");
+                            String message = consoleIn.readLine();
+
+                            if (message.equals("LOGOUT")) {
+                                out.println("LOGOUT");
+                                System.out.println(in.readLine());
+                                break;
+                            } else {
+                                System.out.println("Invalid command.");
+                            }
+                        }
+                    } else {
+                        System.out.println(response);
+                    }
+
                     break;
-                case 3:
+                case "LOGOUT":
                     out.println("LOGOUT");
                     System.out.println(in.readLine());
                     break;
-                case 4:
+                case "VIEW_USERS":
                     out.println("VIEW_USERS");
-                    String response = in.readLine();
-                    System.out.println(response);
+                    System.out.println(in.readLine());
                     break;
                 default:
-                    System.out.println("Invalid choice.");
+                    System.out.println("Invalid command.");
                     break;
             }
         }
     }
-    }
+}
--- src/Server.java
+++ src/Server.java
@@ -4,91 +4,141 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class Server {
-    private static final int PORT = 8888;
-    private static final Map<String, String> users = new HashMap<>();
+    private static final int PORT = 12345;
+
+    private static ArrayList<User> users = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Server started.");
+
+        System.out.println("Server started on port " + PORT);
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            System.out.println("New client connected: " + clientSocket);
 
-            Thread clientThread = new Thread(() -> {
-                try {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                    while (true) {
-                        String message = in.readLine();
-                        if (message.equals("REGISTER")) {
-                            register(in, out);
-                        } else if (message.equals("LOGIN")) {
-                            login(in, out);
-                        } else if (message.equals("LOGOUT")) {
-                            logout(out);
-                        } else if (message.equals("VIEW_USERS")) {
-                            viewUsers(out);
-                        } else {
-                            out.println("Invalid command.");
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        clientSocket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-            clientThread.start();
-        }
-    }
-
-    private static void register(BufferedReader in, PrintWriter out) throws IOException {
-        String name = in.readLine();
-        String username = in.readLine();
-        String password = in.readLine();
-
-        if (users.containsKey(username)) {
-            out.println("Username already taken.");
-        } else {
-            users.put(username, password);
-            out.println("Registration successful.");
-        }
-    }
-
-    private static void login(BufferedReader in, PrintWriter out) throws IOException {
-        String username = in.readLine();
-        String password = in.readLine();
-
-        if (users.containsKey(username) && users.get(username).equals(password)) {
-            out.println("Login successful.");
-        } else {
-            out.println("Invalid username or password.");
-        }
-    }
-
-    private static void logout(PrintWriter out) {
-        out.println("Logout successful.");
-    }
-
-    private static void viewUsers(PrintWriter out) {
-        if (users.isEmpty()) {
-            out.println("No users registered.");
-        } else {
-            out.println("Registered users:");
-            for (String username : users.keySet()) {
-                out.println(username);
-            }
+            ClientHandler clientHandler = new ClientHandler(clientSocket, users);
+            clientHandler.start();
         }
     }
 }
+
+class User {
+    private final String name;
+    private final String username;
+    private final String password;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
+class ClientHandler extends Thread {
+    private final Socket clientSocket;
+    private final ArrayList<User> users;
+
+    public ClientHandler(Socket clientSocket, ArrayList<User> users) {
+        this.clientSocket = clientSocket;
+        this.users = users;
+    }
+
+    @Override
+    public void run() {
+        try (
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            while (true) {
+                String command = in.readLine();
+
+                switch (command) {
+                    case "REGISTER":
+                        String name = in.readLine();
+                        String username = in.readLine();
+                        String password = in.readLine();
+
+                        if (isUserRegistered(username)) {
+                            out.println("Username already exists.");
+                        } else {
+                            users.add(new User(name, username, password));
+                            out.println("User registered successfully.");
+                        }
+
+                        break;
+                    case "LOGIN":
+                        username = in.readLine();
+                        password = in.readLine();
+
+                        if (isUserAuthenticated(username, password)) {
+                            out.println("User authenticated.");
+                        } else {
+                            out.println("Invalid username or password.");
+                        }
+
+                        break;
+                    case "LOGOUT":
+                        out.println("User logged out.");
+                        break;
+                    case "VIEW_USERS":
+                        StringBuilder response = new StringBuilder("Registered users:\n");
+
+                        for (User user : users) {
+                            response.append("Name: ").append(user.getName()).append(", Username: ").append(user.getUsername()).append("\n");
+                        }
+
+                        out.println(response.toString());
+                        break;
+                    default:
+                        out.println("Invalid command.");
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error handling client: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("Error closing client socket: " + e.getMessage());
+            }
+        }
+    }
+
+    private boolean isUserRegistered(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isUserAuthenticated(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+       
